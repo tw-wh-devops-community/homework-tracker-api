767d3e3facd72a5d5626072eb38c36a7c035618c
Panpan Han <hanpanpan200@gmail.com>
2018-01-26 14:22:37 +0800

    [panpan] refactor: code review comments

--- src/db/Seeds.ts
+++ src/db/Seeds.ts
@@ -4,20 +4,24 @@ import { interviewerModel, Interviewer } from '../models/Interviewer'
 const interviewers: interviewerModel[] = require('../../data/interviewer.json') // tslint:disable-line
 
 const initInterviewers = () => {
-  let done = 0
   interviewers.forEach(async (interviewerData) => {
     const interviewer = new Interviewer(interviewerData)
     await interviewer.save()
-    done++
-    console.log(`Importing ${interviewer.name}...`) // tslint:disable-line
-    if (done === interviewers.length) {
-      dbHelper.disconnect().then(() => {
-        console.log(`${interviewers.length} records are imported successfully, database disconnected.`) // tslint:disable-line
-      })
-    }
+  })
+  console.log(`Importing interviewers done.`) // tslint:disable-line
+}
+
+const disconnectDB = () => {
+  dbHelper.disconnect().then(() => {
+    console.log('database disconnected.') // tslint:disable-line
   })
 }
 
 dbHelper.connect().then(() => {
   initInterviewers()
+}).then(() => {
+  disconnectDB()
+}).catch((error) => {
+  console.log('Error when importing data', error)
+  disconnectDB()
 })
