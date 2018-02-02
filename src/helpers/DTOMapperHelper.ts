b5ba3dc1da0e82e42018557a82aeb2d78aeb2b99
Panpan Han <hanpanpan200@gmail.com>
2018-02-02 12:34:09 +0800

    [panpan] refactor: add protection

--- src/helpers/DTOMapperHelper.ts
+++ src/helpers/DTOMapperHelper.ts
@@ -1,6 +1,9 @@
 import * as _ from 'lodash'
 
 const getDTOFromModel = (obj, properties) => {
+  if (!_.isObject(obj)) {
+    return {} as any
+  }
   const dtoObject = _.pick(obj, properties)
   dtoObject.id = obj._id
   return dtoObject
