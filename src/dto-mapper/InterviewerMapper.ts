e28707044195e42175d2568e32433d34cf756612
huhongxiang <gohuhx@163.com>
2018-06-22 10:07:28 +0800

    修改名称

--- src/dto-mapper/InterviewerMapper.ts
+++ src/dto-mapper/InterviewerMapper.ts
@@ -10,7 +10,7 @@ export const mapInterviewer = (interviewer, openIdModel) => {
 
   const openIdProperties = {
     'open_id': openIdModel.open_id,
-    '_openids_id': openIdModel._id,
+    'openids_id': openIdModel._id,
   }
 
   return _.merge(model, openIdProperties) as InterviewerDTO
--- src/dto/Interviewer.ts
+++ src/dto/Interviewer.ts
@@ -5,5 +5,5 @@ export interface InterviewerDTO {
   employee_id: string,
   profile: string,
   open_id: string,
-  _openids_id: string,
+  openids_id: string,
 }
